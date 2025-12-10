bdbff8e5cd0eabd68b97d71ad7460de0343d7eb9
Matthew Brooks <mb964033@gmail.com>
2025-12-10 14:49:47 -0500

    update

--- make.lua
+++ make.lua
@@ -1,11 +1,11 @@
 local env = {
   name = "santoku-resty",
-  version = "0.0.6-1",
+  version = "0.0.7-1",
   license = "MIT",
   public = true,
   dependencies = {
     "lua == 5.1",
-    "santoku >= 0.0.304-1",
+    "santoku >= 0.0.310-1",
     "lua-resty-http == 0.17.2-0",
     "lua-resty-openssl == 1.5.1-1",
     "lua-resty-jwt == 0.2.3-0",
